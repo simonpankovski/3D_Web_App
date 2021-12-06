90eb057c7b81af85102b08b07e608ecf6cbac6f7
simonpankovski <simonpankovski@gmail.com>
2021-12-06 19:01:00 +0100

    added a remote db

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -83,7 +83,10 @@ class RegistrationController extends AbstractController
     public function verifyUserEmail(Request $request, JWTTokenManagerInterface $manager): Response
     {
         // validate email confirmation link, sets User::isVerified=true and persists
-        $decodedToken = $manager->parse($request->get("token"));
+        $decodedToken = ($request->query->has("token")) ? $manager->parse($request->get("token")) : null;
+        if ($decodedToken === null) {
+            return $this->json("The token query parameter is required");
+        }
         $expirationTime = date('Y-m-d H:i:s', $decodedToken['exp'] + 3600);
         if (date('Y-m-d H:i:s', strtotime('1 hour')) > $expirationTime) {
             return $this->json("Expired token, please request a new email validation link!");
